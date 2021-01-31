20c2621a90948d088bd30badffac954363606eae
Victor Casas <victor.casas@outlook.cl>
2021-01-31 15:07:49 -0300

    feat: Create CartContext and addToCart action

---
+++ src/contexts/CartContext.jsx
@@ -0,0 +1,93 @@
+import { createContext, useContext, useReducer } from 'react'
+import PropTypes from 'prop-types'
+
+// contants
+const ADD_TO_CART = 'ADD_TO_CART'
+// const REMOVE_ONE_CART = 'REMOVE_ONE_CART'
+// const REMOVE_ALL_CART = 'REMOVE_ALL_CART'
+
+// initial state
+const initialState = {}
+
+// contexts
+const CartStateContext = createContext()
+const CartDispatchContext = createContext()
+
+// reducer
+function cartReducer(state, action) {
+  const { type, payload } = action
+  const existingCartItem = state[payload.id]
+
+  switch (type) {
+    case ADD_TO_CART: {
+      // If the item exists in the cart
+      if (existingCartItem !== undefined) {
+        const quantity = existingCartItem.quantity + 1
+
+        return {
+          ...state,
+          [payload.id]: {
+            ...existingCartItem,
+            quantity,
+          },
+        }
+      }
+
+      // If the item does NOT exist in the cart
+      return {
+        ...state,
+        [payload.id]: {
+          ...payload,
+          quantity: 1,
+        },
+      }
+    }
+
+    default:
+      throw new Error(`Unhandled action type ${type}`)
+  }
+}
+
+// provider
+const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, initialState)
+  // const [state, dispatch] = useState(initialState)
+
+  return (
+    <CartStateContext.Provider value={state}>
+      <CartDispatchContext.Provider value={dispatch}>
+        {children}
+      </CartDispatchContext.Provider>
+    </CartStateContext.Provider>
+  )
+}
+
+CartProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+// custom hook state
+export const useCartState = () => {
+  const state = useContext(CartStateContext)
+  if (typeof state === 'undefined') {
+    throw new Error('useCartState must be used within a CartProvider')
+  }
+
+  const items = Object.values(state)
+
+  return { items, count: 0, subTotal: 0 }
+}
+
+// custom hook dispatch
+export const useCartDispatch = () => {
+  const dispatch = useContext(CartDispatchContext)
+  if (typeof dispatch === 'undefined') {
+    throw new Error('useCartDispatch must be used within a CartProvider')
+  }
+
+  const addToCart = product => dispatch({ type: ADD_TO_CART, payload: product })
+
+  return { addToCart }
+}
+
+export default CartProvider
